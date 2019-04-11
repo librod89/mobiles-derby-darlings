591c6ef690b2c79c10671d662f07321ba4711009
Libby <libby@supervillains.io>
2019-04-11 11:41:49 -0500

    flesh out more of the home page. get schedule, join and about buttons working

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,7 +1,44 @@
 $(function() {
   if ($("#home").length > 0) {
-    var homeVue = new Vue({
+    new Vue({
       el: '#home',
+      data: {
+        showHomePage: true,
+        showAboutPage: false,
+        showJoinPage: false,
+        showSchedulePage: false
+      },
+      methods: {
+        aboutBtnClick: function(event) {
+          this.setAllPagesToFalse();
+          this.showAboutPage = true;
+          window.location.hash = '#about'
+        },
+        scheduleBtnClick: function() {
+          this.setAllPagesToFalse();
+          this.showSchedulePage = true;
+          window.location.hash = '#schedule'
+        },
+        joinBtnClick: function() {
+          this.setAllPagesToFalse();
+          this.showJoinPage = true;
+          window.location.hash = '#join'
+        },
+        setAllPagesToFalse: function() {
+          this.showHomePage = false;
+          this.showAboutPage = false;
+        }
+      },
+      created: function() {
+        var hash = window.location.hash;
+        if (hash == '#join') {
+          this.joinBtnClick();
+        } else if (hash == '#about') {
+          this.aboutBtnClick();
+        } else if (hash == '#schedule') {
+          this.scheduleBtnClick();
+        }
+      }
     });
   }
 });
